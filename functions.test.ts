dfb5f79bfedfcbfe78bb39f2659dce3c3e8d9b87
Joseph Al-Abudi <josephalabudi@gmail.com>
2022-04-29 16:56:36 -0600

    fixed my functions test file

--- functions.test.ts
+++ functions.test.ts
@@ -2,15 +2,16 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     test('return an array', () => {
-        expect(shuffleArray).not.toBeNull();
+        expect(Array.isArray(shuffleArray(testArr))).toBe(true)
     })
     test('return an array of the same length as the arg sent in', () => {
-        expect(shuffleArray).toHaveLength(shuffleArray)
+        expect(shuffleArray(testArr)).toHaveLength(testArr.length)
     })
     test('have all the same items in the array', () => {
-        expect(shuffleArray).toContainEqual(shuffleArray)
+        expect(shuffleArray(testArr)).toEqual(expect.arrayContaining(testArr))
     })
     test('have shuffled around the items', () => {
-        expect(shuffleArray).toHaveReturned
+       let result = shuffleArray(testArr)
+       expect(result.join()).not.toEqual(testArr.join())
     })
 })
